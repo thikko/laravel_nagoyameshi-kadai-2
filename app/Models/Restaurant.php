eecf2c253921510721fb28e3ce4baf379d3903b6
thikko <aosaku706@icloud.com>
2025-02-21 04:03:41 +0900

    店舗にカテゴリを設定

--- app/Models/Restaurant.php
+++ app/Models/Restaurant.php
@@ -4,8 +4,27 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Kyslik\ColumnSortable\Sortable;
 
 class Restaurant extends Model
 {
-    use HasFactory;
+    use HasFactory, Sortable;
+
+    protected $casts = [
+        'created_at' => 'datetime:Y-m-d H:i:s O',
+        'updated_at' => 'datetime:Y-m-d H:i:s O',
+    ];
+
+    protected function serializeDate(\DateTimeInterface $date)
+    {
+        return $date->setTimezone(new \DateTimeZone('+09:00'))->format('Y-m-d H:i:s');
+    }
+
+    public function categories() {
+        return $this->belongsToMany(Category::class)->withTimestamps();
+    }
+
+    public function regular_Holidays() {
+        return $this->belongsToMany(RegularHoliday::class)->withTimestamps();
+    }
 }
